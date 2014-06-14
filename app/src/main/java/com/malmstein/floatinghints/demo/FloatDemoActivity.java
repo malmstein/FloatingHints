e971ed44bae08ae6b4b19084d0da8940ffe500cf
David Gonzalez <david@novoda.com>
2014-06-14 19:47:36 +0200

    added demo module

---
+++ app/src/main/java/com/malmstein/floatinghints/demo/FloatDemoActivity.java
@@ -0,0 +1,19 @@
+package com.malmstein.floatinghints.demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FloatDemoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_float);
+    }
+
+
+
+
+
+}
